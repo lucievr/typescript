d16566326533949ab1d83a0563a38372ab253c1f
lucievr <lucy.vrsovska@gmail.com>
2020-06-14 21:18:27 +0200

    Type aliases, interfaces and generic types

--- typescript-basics/app.js
+++ typescript-basics/app.js
@@ -1,15 +1,44 @@
-var num1Element = document.getElementById('num1'); // type casting, ts can't tell it's an input from its id
-var num2Element = document.getElementById('num2');
-var buttonElement = document.querySelector('button');
+"use strict";
+const num1Element = document.getElementById('num1'); // type casting, ts can't tell it's an input from its id
+const num2Element = document.getElementById('num2');
+// add ! in strict mode, ts doesn't know if the button element actually exists and throws an error when adding event listener to it
+const buttonElement = document.querySelector('button');
 // HTMLButtonElement is inferred type here since we're using querySelector with 'button'
 // const buttonElement: HTMLButtonElement = document.querySelector('button');
+// const numResults: Array<number> = []; // number, the value inside the array is the generic type
+const numResults = [];
+const textResults = [];
 function add(num1, num2) {
-    return num1 + num2;
+    // union type - number or string
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2; // force it to a number
 }
-;
-buttonElement.addEventListener('click', function () {
-    var num1 = num1Element.value; // input value always returns a string!
-    var num2 = num2Element.value;
-    var result = add(+num1, +num2); // we need to convert it to a number
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
+buttonElement.addEventListener('click', () => {
+    const num1 = num1Element.value; // input value always returns a string!
+    const num2 = num2Element.value;
+    const result = add(+num1, +num2);
+    numResults.push(result);
+    const stringResult = add(num1, num2);
+    textResults.push(stringResult);
     console.log(result);
+    console.log(stringResult);
+    printResult({ val: result, timestamp: new Date() });
+    console.log(numResults, textResults);
+});
+// this promise returns a string which is a generic type
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('It worked');
+    }, 1000);
+});
+myPromise.then((result) => {
+    console.log(result.split(' '));
 });
--- typescript-basics/app.ts
+++ typescript-basics/app.ts
@@ -5,13 +5,52 @@ const buttonElement = document.querySelector('button')!;
 // HTMLButtonElement is inferred type here since we're using querySelector with 'button'
 // const buttonElement: HTMLButtonElement = document.querySelector('button');
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
-};
+// const numResults: Array<number> = []; // number, the value inside the array is the generic type
+const numResults: number[] = [];
+const textResults: string[] = [];
+
+type NumOrString = number | string; // we can define our type aliases (=> cleaner code)
+// type Result = { val: number; timestamp: Date };
+
+interface ResultObj {
+  val: number;
+  timestamp: Date;
+}
+
+function add(num1: NumOrString, num2: NumOrString) {
+  // union type - number or string
+  if (typeof num1 === 'number' && typeof num2 === 'number') {
+    return num1 + num2;
+  } else if (typeof num1 === 'string' && typeof num2 === 'string') {
+    return num1 + ' ' + num2;
+  }
+  return +num1 + +num2; // force it to a number
+}
+
+function printResult(resultObj: ResultObj) {
+  console.log(resultObj.val);
+}
 
 buttonElement.addEventListener('click', () => {
   const num1 = num1Element.value; // input value always returns a string!
   const num2 = num2Element.value;
-  const result = add(+num1, +num2); // we need to convert it to a number
+  const result = add(+num1, +num2);
+  numResults.push(result as number);
+  const stringResult = add(num1, num2);
+  textResults.push(stringResult as string);
   console.log(result);
+  console.log(stringResult);
+  printResult({ val: result as number, timestamp: new Date() });
+  console.log(numResults, textResults);
+});
+
+// this promise returns a string which is a generic type
+const myPromise = new Promise<string>((resolve, reject) => {
+  setTimeout(() => {
+    resolve('It worked');
+  }, 1000);
+});
+
+myPromise.then((result) => {
+  console.log(result.split(' '));
 });
