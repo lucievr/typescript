67e1a29748f943a9f1f981688e1295364724569b
lucievr <lucy.vrsovska@gmail.com>
2020-06-24 23:42:30 +0200

    Implement sorting for linked list

--- typescript-sort-app/src/index.ts
+++ typescript-sort-app/src/index.ts
@@ -1,13 +1,24 @@
 import { Sorter } from './Sorter';
-import { NumbersCollection } from './NumbersCollection';
-import { CharactersCollection } from './CharactersCollection'
+// import { NumbersCollection } from './NumbersCollection';
+// import { CharactersCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([100, 30, -5, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('XgNam');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('XgNam');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(4);
+linkedList.add(-3);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
